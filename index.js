4abc81941616c6ebeb35d295d5e58f00a437ee75
c.giacometti <camilla.giacometti@gmail.com>
2022-06-19 09:53:33 -0300

    Rotas estabelecidas

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const server = express();
 
 server.use(cors());
 
+let tweets = [];
+
 server.post('/sign-up', (req, res) => {
     res.send('alo galera de piao alo galera de cowboy');
 })
 
-server.listen(4000);
+server.get('/tweets', (req, res) => {
+    res.send(tweets);
+})
+
+server.post('/tweets', (req, res) => {
+
+})
+
+server.listen(5000);
