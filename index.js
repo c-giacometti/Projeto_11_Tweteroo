695e8c9f515b5fbaf4721cd9da611742c9135a5f
c.giacometti <camilla.giacometti@gmail.com>
2022-06-19 10:45:02 -0300

    requisitos basicos

--- index.js
+++ index.js
@@ -2,20 +2,44 @@ import express from 'express';
 import cors from 'cors';
 
 const server = express();
-
 server.use(cors());
+server.use(express.json());
 
 let tweets = [];
+let user;
 
 server.post('/sign-up', (req, res) => {
-    res.send('alo galera de piao alo galera de cowboy');
+
+    user = [];
+
+    user.push(
+        {
+            username: req.body.username,
+            avatar: req.body.avatar
+        }
+    );
+
+    res.send('ok');
+
 })
 
 server.get('/tweets', (req, res) => {
+
     res.send(tweets);
+
 })
 
 server.post('/tweets', (req, res) => {
+    
+    tweets.push(
+        {
+            username: req.body.username,
+            avatar: user[0].avatar,
+            tweet: req.body.tweet
+        }
+    );
+
+    res.send('ok');
 
 })
 
